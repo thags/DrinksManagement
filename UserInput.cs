5b4cc78495b3707ad90d3d64f6f874bd4256acdf
Tyler <tyler.hagstrom@gmail.com>
2021-12-12 09:33:52 -0600

    Add UserInput Interface, App.Config and UserInput class
    Created a UserInput interface, just to test interfaces a bit. Implemented the interface in the UserInput class. Built code in UserInput class to Try to get user input string, a method to check that the user input string is part of a correct choice, and also a method to rip a string of formatting - which for now just removes all spaces and makes it all lower case.

---
+++ UserInput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksManagement
+{
+    class UserInput : IUserInput
+    {
+        public bool CheckUserInput(string userInput, List<string> availableOptions)
+        {
+            string unFormatUserInput = RipStringOfFormatting(userInput);
+            foreach (string option in availableOptions)
+            {
+                string unFormatOption = RipStringOfFormatting(option);
+                if (unFormatOption == unFormatUserInput)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool TryGetUserInputString(out string userInput, int numAllowedAttempts = 5)
+        {
+            int totalAttempts = 0;
+            do
+            {
+                userInput = Console.ReadLine();
+                totalAttempts++;
+            } while(totalAttempts <= numAllowedAttempts);
+
+            
+
+            throw new NotImplementedException();
+        }
+        private string RipStringOfFormatting(string s)
+        {
+            s = s.Replace(" ", "");
+            s = s.ToLower();
+            return s;
+        }
+    }
+}
