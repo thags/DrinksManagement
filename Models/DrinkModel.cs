4ad80b3ff598a90d56cdb9e616a04775618ec9ea
Tyler <tyler.hagstrom@gmail.com>
2021-12-12 13:06:05 -0600

    Add DrinkModel
    Added a DrinkModel that will take in the Drink Names and Drink Id's returned by the API

---
+++ Models/DrinkModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksManagement.Models
+{
+    //This should be used to create Drink object from the API call to TheCocktailDB
+    class DrinkModel
+    {
+        public string DrinkName { get; set; }
+        public int DrinkId { get; set; }
+    }
+}
