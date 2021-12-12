4701c98772a246b9dea5c4d8ed95965525dd9966
Tyler <tyler.hagstrom@gmail.com>
2021-12-12 08:36:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrinksManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
